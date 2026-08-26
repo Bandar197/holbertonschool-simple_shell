c3bd25b0e5d4904a6e1939758332a6fe0b6a894d
Bandar197 <bandar14141489@gmail.com>
2026-08-26 20:15:15 +0300

    Fix formatting and separate function declarations in shell.h

--- shell.h
+++ shell.h
@@ -12,7 +12,8 @@
 extern char **environ;
 
 void run_shell(char *program_name);
-int execute_cmd(char **args, char *program_name, int count);char **tokenize_line(char *line);
+int execute_cmd(char **args, char *program_name, int count);
+char **tokenize_line(char *line);
 char *get_path(void);
 char *find_command(char *command);
 char *build_path(char *dir, char *command);
